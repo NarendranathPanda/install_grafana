9dfda51f761798ee6eebdce9d1e28f523ae30b59
Narendranath Panda <NarendranathPanda@users.noreply.github.com>
2019-12-02 09:56:52 +0530

    Update install_grafana.sh

--- install_grafana.sh
+++ install_grafana.sh
@@ -2,3 +2,7 @@ yum update -y;
 echo "yum Updated.";
 
 cd /tmp;
+sudo su;
+sudo wget https://dl.grafana.com/oss/release/grafana-6.5.1-1.x86_64.rpm
+sudo yum localinstall grafana-6.5.1-1.x86_64.rpm
+echo "grafana installed"
